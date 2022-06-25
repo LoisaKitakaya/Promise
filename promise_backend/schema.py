cf2845209827a23e6066adadaabc1f53a2ff2d84
Freedom Loisa <kitakayaloisa@gmail.com>
2022-06-25 22:16:36 +0300

    installed cloudinary

--- promise_backend/schema.py
+++ promise_backend/schema.py
@@ -1,47 +1,8 @@
 import graphene
 import graphql_jwt
-from users.models import User
 import commerce.schema as commerce_schema
-from graphene_django import DjangoObjectType
-
-class UserType(DjangoObjectType):
-
-    class Meta:
-
-        model = User
-
-        fields = '__all__'
-
-class UserMutation(graphene.Mutation):
-
-    class Arguments:
-
-        username = graphene.String(required=True)
-
-        email = graphene.String(required=True)
-
-        first_name = graphene.String(required=True)
-
-        last_name = graphene.String(required=True)
-
-        password = graphene.String(required=True)
-
-    user = graphene.Field(UserType)
-
-    @classmethod
-    def mutate(cls, root, info, username, email, first_name, last_name, password):
-
-        user = User(username=username, email=email, first_name=first_name, last_name=last_name)
-
-        user.set_password(password)
-
-        user.save()
-
-        return UserMutation(user=user)
 
 class Mutation(graphene.ObjectType):
-
-    create_user = UserMutation.Field()
 
     token_auth = graphql_jwt.ObtainJSONWebToken.Field()
 
@@ -53,10 +14,6 @@ class Mutation(graphene.ObjectType):
 
 class Query(commerce_schema.Query, graphene.ObjectType):
 
-    all_users = graphene.List(UserType)
-
-    def resolve_all_users(root, info):
-
-        return User.objects.all()
+    pass
 
 schema = graphene.Schema(mutation=Mutation, query=Query)
